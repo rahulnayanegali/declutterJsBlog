52830eb375ec059eb2e8cfaf74c7fdd5d426f35e
Rahul Nayanegali <rahulnayanegali@gmail.com>
2020-06-05 23:35:02 +0530

    Fetching API

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function declutter() {
 	console.log('Declutter JS, Just Reloaded')
-
 	// Menu Toggle
-
-
 }
 
 const toggleMenu = () => {
@@ -22,6 +19,22 @@ const toggleMenu = () => {
 	}
 window.onload = declutter();
 
+
+// Menu Toggle for small screens using hamburger icon
 const menuToggle = () => {
 		document.querySelector('.nav-bar').style.display = 'block';
 	}
+
+
+// Fetching Data from api
+
+/*
+fetch('https://forkify-api.herokuapp.com/api/search?&q=pizza')
+	.then(response => response.json())
+	.then(data => console.log(data));
+*/
+
+let html = `<h1>Hello World!</h1>`
+let results = document.querySelector('.results');
+
+//results.insertAdjacentHTML('beforeend', html);

27823ef992bed75722e3ea7ee5210a2910f8fadb
Rahul Nayanegali <rahulnayanegali@gmail.com>
2020-06-12 00:24:46 +0530

    testdb implement

--- index.js
+++ index.js
@@ -14,48 +14,56 @@ const toggleMenu = () => {
 		}
 	}
 
-/*
-// Checking Purpose
-const url = "https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=9f1312f30bfe42d3ae1237ea14e28329";
-fetch(url, {
-	mode: 'cors'
-})
-*/
+
 	// Calling News API
 
 let ress;
 
+/*
+fetch('https://rantestdb-4bfa.restdb.io/rest/contact?apikey=5ee2654b4e6043315b0af9c7')
+	.then(res => res.json())
+	.then(data => console.log(data))
+*/
 
 async function getResults() {
 	try {
-		const res = await fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=9f1312f30bfe42d3ae1237ea14e28329')
+		const res = await fetch('https://rantestdb-4bfa.restdb.io/rest/contact?apikey=5ee2654b4e6043315b0af9c7')
 	.then(response => response.json())
-	.then(data => ress = data.articles);
+	.then(data => ress = data);
 	// this.recipes = res.data;
 	} catch (error) {
 		alert(error)
 	}
 
 	console.log(ress)
-	// renderResults(ress);
+	renderResults(ress);
 }
  getResults()
 
-let results = document.querySelector('.results__list');
+let results = document.querySelector('.leftcolumn');
 
 //results.insertAdjacentHTML('beforeend', html);
 
 
 let renderArticle = article => {
+	let time = article.publishedAt;
+	let photoKey = article.photo;
+	// console.log(time.toLocaleString())
     const markup = `
-    <li>
-		<div class="card">
-	      <h2>${article.title}</h2>
-	      <img height=100vh, width=100vw, src=${article.urlToImage}>
-	      <h4>${article.author}</h4>
-	      <textarea>${article.description}</textarea>
-		</div>
-	</li>
+    <div class="card">
+		          <h2>${article.name}</h2>
+		          <h5>Title description, Dec 7, 2017</h5>
+		          <div class="articleimg">
+		          <img src='https://rantestdb-4bfa.restdb.io/media/${photoKey}' height=300px width=100% >
+		          </div>
+		          <p>Some text..</p>
+		          <p>
+		            Sunt in culpa qui officia deserunt mollit anim id est laborum
+		            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+		            exercitation ullamco.
+		          </p>
+		        </div>
     
     `;
     results.insertAdjacentHTML('beforeend', markup);

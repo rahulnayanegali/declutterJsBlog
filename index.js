4071aca54d3c30cebbe3ad382c90acf77d445bf9
Rahul Nayanegali <rahulnayanegali@gmail.com>
2020-09-01 22:06:51 +0530

    articles expand

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// Variable Declaration
+let ress;
+let leftcolumn = document.querySelector('.leftcolumn');
+
 // Methods
 function declutter() {
 	console.log('Declutter JS, Just Reloaded')
 }
-	// Menu Toggle
+// Menu Toggle
 const toggleMenu = () => {
 		let hamburgerMenu = document.querySelector(".hamburger-btn");
 		let navBar = document.querySelector(".nav-bar");
@@ -14,62 +18,89 @@ const toggleMenu = () => {
 		}
 	}
 
+let renderResults = articles => {
+	// console.log(renderResults);
+	// apply ternery operator
+	articles.forEach(renderArticles)
+};
 
-	// Calling News API
-
-let ress;
-
-/*
-fetch('https://rantestdb-4bfa.restdb.io/rest/contact?apikey=5ee2654b4e6043315b0af9c7')
-	.then(res => res.json())
-	.then(data => console.log(data))
-*/
-
-async function getResults() {
-	try {
-		const res = await fetch('https://declutterjsarticles-673c.restdb.io/rest/articles?apikey=5ee3001d4e6043315b0af9f8')
-	.then(response => response.json())
-	.then(data => ress = data);
-	// this.recipes = res.data;
-	} catch (error) {
-		alert(error)
-	}
-
-	console.log(ress)
-	renderResults(ress);
+// Finding  URL Path
+const parseLocation = () => {
+	let hash = window.location.hash.substr(1);
+	hash = hash.replace('/', "");
+	hash = hash.replace(/%20/g, " ");
+	return hash || '/'
 }
- getResults()
+// console.log(parseLocation())
 
-let results = document.querySelector('.leftcolumn');
+// Finding article by path
+const findArticelByPath = (articles, path) => {
+	return articles.find( article => article.title === path)
+}
 
-//results.insertAdjacentHTML('beforeend', html);
-
-
-let renderArticle = article => {
-	let time = article.publishedAt;
+// Function to render all articles
+const renderArticles = article => {
+	// let time = article.publishedAt;
+	console.log(article);
 	let photoKey = article.image;
-	let date = article.date;
-	console.log(date);
+	let date = new Date(article.date);
     const markup = `
     <div class="card">
 		          <div class="articleimg">
 		          <img src='https://declutterjsarticles-673c.restdb.io/media/${photoKey}' height=300px width=100% >
 		          </div>
-		          <a href="#"><h1>${article.title}</h1></a>
-		          <h5> 07/06/2020</h5>
-		          <p>${article.description}</p>
-		        </div>
-    
-    `;
-    results.insertAdjacentHTML('beforeend', markup);
+		          <a href="#/${article.title}"><h1>${article.title}</h1></a>
+				  <h5>${date.toLocaleDateString()}</h5>
+				  <p>${article.description}</p>
+	</div>
+	`;
+	leftcolumn.insertAdjacentHTML('beforeend', markup);
+	// selectedArticle.innerHTML = markup
 };
 
-let renderResults = articles => {
-    // console.log(renderResults);
-    
-    articles.forEach(renderArticle)
-};
+// Function to render selected article
+let renderClickedArticle = article => {
+	// let time = article.publishedAt;
+	console.log(article);
+	let photoKey = article.image;
+	let date = new Date(article.date);
+	let converter = new showdown.Converter();
+	let md = article.content
+	// var md = article.content;
+	let html = converter.makeHtml(md);
+	leftcolumn.innerHTML = `
+	<div class="article-style">
+	${html}
+	</div>
+	`
+	};
 
+// API Calling
+async function getResults() {
+	console.log('called')
+	leftcolumn.innerHTML = '';
+	try {
+		const res = await fetch('https://declutterjsarticles-673c.restdb.io/rest/articles?apikey=5ee3001d4e6043315b0af9f8')
+	.then(response => response.json())
+	.then(data => ress = data);
+	} catch (error) {
+		alert(error)
+	}
+	if (parseLocation() === '/') {
+		renderResults(ress);
+	} else {
+		let path = parseLocation();
+		let article = findArticelByPath(ress, path);
+		renderClickedArticle(article);
+	}
+}
 
+//results.insertAdjacentHTML('beforeend', html);	
 // Method Calling
 window.onload = declutter();
+getResults()
+
+// document.onload = getResults()
+
+window.addEventListener('hashchange', getResults);
+// window.addEventListener('load', getResults());
